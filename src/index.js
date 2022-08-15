d2d65408d6a165661e95740d6e8309db88ae0165
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-08-15 12:21:46 -0300

    feat: improving user's feedback on error, 'file doesn't exist'

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import fs from 'fs'
 async function readFile(path) {
     try {
         const text = await fs.promises.readFile(path, 'utf-8')
-        console.log(text)
+        console.log(linkExtractor(text))
     } catch (error) {
-        throw new Error(chalk.red(error))
+        throw new Error(chalk.red(error.code, 'this file doesn\'t exist in this path'))
     }
 }
 
@@ -16,12 +16,10 @@ function linkExtractor(markDownText) {
     let link
     // Loop through each occurrence
     while ((link = regex.exec(markDownText)) != null) {
-        links[link[1]] = link[2]
+        links[link[1]] = link[2] 
+        // Regex: group 1 -- group2
     }
     return links
 }
 
-const text = "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)"
-console.log(linkExtractor(text))
-
-readFile('./src/files/text1.md')
+readFile('./src/files/text2.md')
