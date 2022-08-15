ea02622bbb34ac230c8b0d3b0b6529cfa72e19a8
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-08-15 17:01:57 -0300

    test: shoLinks

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,5 +1,17 @@
 import showLinks from '../index.js'
 
-test('must be a fucntion', () => {
-    expect(typeof showLinks).toBe('function')
+describe('showLinks::', () => {
+    it('must be a function', () => {
+        expect(typeof showLinks).toBe('function')
+    })
+
+    it('if the .md has links, it must return them all', async () => {
+        const path = '/home/samuel/Desktop/lib-markdown/src/files/text1.md'
+        expect(typeof await showLinks(path)).toBe('object')
+    })
+
+    it('if the .md has no links, it must say it', async () => {
+        const path = '/home/samuel/Desktop/lib-markdown/src/files/text1_nolinks.md'
+        expect(typeof await showLinks(path)).toBe('string')
+    })
 })
